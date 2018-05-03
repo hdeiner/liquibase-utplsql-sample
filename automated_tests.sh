e5394ed50047c8a4de6f6852c77e95d96858ef7e
hdeiner <howard.deiner@deinersoft.com>
2018-05-03 14:39:13 -0400

    Create automated_tests.sh to have one basg script that goes from birth to grave on creating a CRISP utPLSQL tested database.
    TODO: The PLSQL code that is tested has nothing to do with the schema and test data that Liquibase installs.  That must be fixed!

---
+++ automated_tests.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo Stop all Docker containers
+sudo docker stop $(sudo docker ps -aq)
+
+echo Remove exited Docker containers
+sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo docker rm -v
+
+echo Remove unused Docker images
+sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r sudo docker rmi
+
+echo Run the Docker image for Oracle 11g XE
+sudo docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 alexeiled/docker-oracle-xe-11g
+
+echo Wait for Oracle to start up
+sleep 60
+
+echo Install Schema and Test Data
+mvn install
+
+echo Install utPLSQL
+cd utPLSQL/source
+sqlplus sys/oracle@localhost:49161/xe as sysdba @install_headless.sql
+cd ../..
+
+echo Install PLSQL source to test
+sqlplus -L -S system/oracle@localhost:49161/xe <<SQL
+whenever sqlerror exit failure rollback
+whenever oserror  exit failure rollback
+
+@demoPLSQL/source/award_bonus/employees_test.sql
+@demoPLSQL/source/award_bonus/award_bonus.prc
+
+@demoPLSQL/source/between_string/betwnstr.fnc
+
+@demoPLSQL/source/remove_rooms_by_name/rooms.sql
+@demoPLSQL/source/remove_rooms_by_name/remove_rooms_by_name.prc
+
+exit
+SQL
+
+echo Install PLSQL unit tests
+sqlplus -L -S system/oracle@localhost:49161/xe <<SQL
+whenever sqlerror exit failure rollback
+whenever oserror  exit failure rollback
+
+@demoPLSQL/test/award_bonus/test_award_bonus.pks
+@demoPLSQL/test/award_bonus/test_award_bonus.pkb
+
+@demoPLSQL/test/between_string/test_betwnstr.pks
+@demoPLSQL/test/between_string/test_betwnstr.pkb
+
+@demoPLSQL/test/remove_rooms_by_name/test_remove_rooms_by_name.pks
+@demoPLSQL/test/remove_rooms_by_name/test_remove_rooms_by_name.pkb
+
+exit
+SQL
+
+echo Run the PLSQL unit tests
+utplsql run system/oracle@localhost:49161:xe \
+-source_path=demoPLSQL/source -test_path=demoPLSQL/test \
+-f=ut_documentation_reporter  -c \
+-f=ut_coverage_sonar_reporter -o=coverage.xml \
+-f=ut_sonar_test_reporter     -o=test_results.xml \
+--failure-exit-code=0
--- readme.md
+++ readme.md
@@ -18,7 +18,31 @@ sudo docker pull alexeiled/docker-oracle-xe-11g
 Run the Docker image.
 sudo docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 alexeiled/docker-oracle-xe-11g
 
+Stop all Docker containers
+sudo docker stop $(sudo docker ps -aq)
 
 Install utPLSQL
+cd utPLSQL/source
 sqlplus sys/oracle@localhost:49161/xe as sysdba @install_headless.sql
+
+Install utPLSQL-cli
+example invocation
+utplsql run system/oracle@localhost:49161:xe
+
+The following is not what we want.  That user and password, installed by utPLSQL @install_headless.sql will come into play someday...
+utplsql run UT3/XNtxj8eEgA6X6b6f@localhost:49161:xe
+
+The warning of "Warning: Could not find Oracle i18n driver in classpath. Depending on the database charset utPLSQL-cli might not run properly. It is recommended you download the i18n driver from the Oracle website and copy it to the 'lib' folder of your utPLSQL-cli installation." is not satisfied with i18n.jar.  You need to download the orai18n.jar to make this work, at least for Java 8. 
+
+Install the PLSQL code to test
+cd utPLSQL-demo-project/source
+install_sh
+
+Install the PLSQL unit tests
+cd utPLSQL-demo-project/test
+install_sh
+
+Run the tests
+cd utPLSQL-demo-project/test
+run_sh
 
