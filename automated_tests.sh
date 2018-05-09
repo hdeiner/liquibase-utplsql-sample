c89e6e4fb737c1398e855d4b6bee51f5c4f7be1a
hdeiner <howard.deiner@deinersoft.com>
2018-05-09 15:18:34 -0400

    Fix automated_tests.sh so that Liquibase is used the way it is supposed to be (no stuffing PLQSL, etc, through sqlplus).  Also, refactored to have a changelog for the schema and data USED by a changelog that includes unit tests.  That way, we start a path for how Liquibase and this whole process can support production databases.

--- automated_tests.sh
+++ automated_tests.sh
@@ -12,49 +12,16 @@ sudo -S <<< "password" docker images --no-trunc | grep '<none>' | awk '{ print $
 echo Run the Docker image for Oracle 11g XE
 sudo -S <<< "password" docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 alexeiled/docker-oracle-xe-11g
 
-echo Wait for Oracle to start up
+echo Pause a minute to allow Oracle to start up
 sleep 60
-
-echo Install Schema and Test Data
-liquibase --changeLogFile=db-changelog-schema-and-testdata.xml update
 
 echo Install utPLSQL
 cd utPLSQL/source
 sqlplus sys/oracle@localhost:49161/xe as sysdba @install_headless.sql
 cd ../..
 
-echo Install PLSQL source to test
-sqlplus -L -S system/oracle@localhost:49161/xe <<SQL
-whenever sqlerror exit failure rollback
-whenever oserror  exit failure rollback
-
-@demoPLSQL/source/award_bonus/employees_test.sql
-@demoPLSQL/source/award_bonus/award_bonus.prc
-
-@demoPLSQL/source/between_string/betwnstr.fnc
-
-@demoPLSQL/source/remove_rooms_by_name/rooms.sql
-@demoPLSQL/source/remove_rooms_by_name/remove_rooms_by_name.prc
-
-exit
-SQL
-
-echo Install PLSQL unit tests
-sqlplus -L -S system/oracle@localhost:49161/xe <<SQL
-whenever sqlerror exit failure rollback
-whenever oserror  exit failure rollback
-
-@demoPLSQL/test/award_bonus/test_award_bonus.pks
-@demoPLSQL/test/award_bonus/test_award_bonus.pkb
-
-@demoPLSQL/test/between_string/test_betwnstr.pks
-@demoPLSQL/test/between_string/test_betwnstr.pkb
-
-@demoPLSQL/test/remove_rooms_by_name/test_remove_rooms_by_name.pks
-@demoPLSQL/test/remove_rooms_by_name/test_remove_rooms_by_name.pkb
-
-exit
-SQL
+echo Install Schema, Test Data, PLSQL Code and utPLSQL to test with
+liquibase --changeLogFile=db-changelog-PLSQL-and-unit-tests.xml update
 
 echo Run the PLSQL unit tests
 mkdir -p test_results
