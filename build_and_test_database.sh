77fbd50f77a2e1b9f053cc25b6aac509cd8d019b
hdeiner <howard.deiner@deinersoft.com>
2018-05-10 10:09:38 -0400

    Invoke ut_coverage_html_reporter when utplsql is run.

--- build_and_test_database.sh
+++ build_and_test_database.sh
@@ -18,6 +18,7 @@ mkdir -p test_results
 utplsql run system/oracle@localhost:49161:xe \
 -source_path=src/main/db -test_path=src/test/db \
 -f=ut_documentation_reporter  -c \
+-f=ut_coverage_html_reporter  -o=test_results/coverage.html \
 -f=ut_coverage_sonar_reporter -o=test_results/sonar_coverage.xml \
 -f=ut_sonar_test_reporter     -o=test_results/sonar_test.xml \
 -f=ut_xunit_reporter          -o=test_results/xunit_results.xml \
